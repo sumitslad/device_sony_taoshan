027f2fd7f54aae10c0bd85bb50666ceb445b73dc
CodeZero <sohamlad7@gmail.com>
2015-06-23 20:05:23 +0530

    taoshan : AOSP Bringup

--- aosp_taoshan.mk
+++ aosp_taoshan.mk
@@ -12,24 +12,19 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
+# Inherit from the common Open Source product configuration
+$(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
+
+# Inherit from hardware-specific part of the product configuration
 $(call inherit-product, device/sony/taoshan/taoshan.mk)
-
-# Enhanced NFC
-$(call inherit-product, vendor/cm/config/nfc_enhanced.mk)
-
-# Inherit CM common Phone stuff.
-$(call inherit-product, vendor/cm/config/common_full_phone.mk)
-
-# Boot Animation
-TARGET_SCREEN_HEIGHT := 854
-TARGET_SCREEN_WIDTH := 480
+$(call inherit-product-if-exists, vendor/sony/taoshan/taoshan-vendor.mk)
 
 # Build fingerprints
 PRODUCT_BUILD_PROP_OVERRIDES += PRODUCT_NAME=C2105 BUILD_FINGERPRINT=Sony/C2105/C2105:4.2.2/15.3.A.1.17/Android.1016:user/release-keys PRIVATE_BUILD_DESC="C2105-user 4.2.2 JDQ39 Android.1016 test-keys"
 
 # Device identifier
 PRODUCT_DEVICE := taoshan
-PRODUCT_NAME := cm_taoshan
+PRODUCT_NAME := aosp_taoshan
 PRODUCT_BRAND := Sony
 PRODUCT_MODEL := C2105
 PRODUCT_MANUFACTURER := Sony
